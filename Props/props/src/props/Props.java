cbf453010817009194b6c60748f4a62b5efe6162
Pascal Emile Fares <pfares@cofares.net>
2016-02-05 12:37:05 +0200

    Version de la vidéo https://youtu.be/AOiONm0thJ8

--- Props/props/src/props/Props.java
+++ Props/props/src/props/Props.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * fichier de propriétés en Java 
  */
 package props;
 
@@ -28,7 +26,7 @@ public class Props {
         
         System.out.println("config.file="+propFile);
         
-        System.out.println(args[0]+"="+sprop.getProperty(args[0]));
+        System.out.println(args[0]+"="+System.getenv(args[0]));
         //Read my own property file
         
         Properties myprop = new Properties();
