cbde67ca162f8c1d3a5a2d3500a879d03da5518f
Pascal Emile Fares <pfares@cofares.net>
2016-04-04 16:40:00 +0300

    Rename and refactor

--- Lambdas/Aproche9_AllGeneric/src/aproche4/Aproche4.java
+++ Lambdas/Aproche9_AllGeneric/src/aproche4/Aproche4.java
@@ -7,9 +7,6 @@ package aproche4;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import net.cofares.ljug.Person;
 
 /**
