ad67ca24ebb4be60ac73242f958da0e726c15319
Pascal Emile Fares <pfares@cofares.net>
2016-04-04 15:52:51 +0300

    Aproche4

---
+++ Lambdas/Aproche4_AnonInterfacece/src/aproche4/CheckPerson.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aproche4;
+
+import net.cofares.ljug.Person;
+
+/**
+ *
+ * @author pascalfares
+ */
+public interface CheckPerson {
+     boolean test(Person p);
+}
