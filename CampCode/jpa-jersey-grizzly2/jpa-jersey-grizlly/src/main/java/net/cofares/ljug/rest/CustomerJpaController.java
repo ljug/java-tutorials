e58ad03c9c1291427eac9074caaa096824f2c65e
Pascal Fares Domicile <pascal.fares@cofares.net>
2017-12-03 20:39:18 +0200

    jpa-jersey-grizlly

---
+++ CampCode/jpa-jersey-grizzly2/jpa-jersey-grizlly/src/main/java/net/cofares/ljug/rest/CustomerJpaController.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.cofares.ljug.rest;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import net.cofares.ljug.rest.exceptions.NonexistentEntityException;
+import net.cofares.ljug.rest.exceptions.PreexistingEntityException;
+
+/**
+ *
+ * @author pfares
+ */
+public class CustomerJpaController implements Serializable {
+
+    public CustomerJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Customer customer) throws PreexistingEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(customer);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            if (findCustomer(customer.getId()) != null) {
+                throw new PreexistingEntityException("Customer " + customer + " already exists.", ex);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Customer customer) throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            customer = em.merge(customer);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                long id = customer.getId();
+                if (findCustomer(id) == null) {
+                    throw new NonexistentEntityException("The customer with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(long id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Customer customer;
+            try {
+                customer = em.getReference(Customer.class, id);
+                customer.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The customer with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(customer);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Customer> findCustomerEntities() {
+        return findCustomerEntities(true, -1, -1);
+    }
+
+    public List<Customer> findCustomerEntities(int maxResults, int firstResult) {
+        return findCustomerEntities(false, maxResults, firstResult);
+    }
+
+    private List<Customer> findCustomerEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Customer.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Customer findCustomer(long id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Customer.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getCustomerCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Customer> rt = cq.from(Customer.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
