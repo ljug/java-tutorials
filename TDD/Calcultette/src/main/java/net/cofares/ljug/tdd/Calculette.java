d2daaa6db39d88104cf017b2bcfb38b929cb5b0c
Pascal Fares Domicile <pascal.fares@cofares.net>
2018-10-18 07:43:24 +0300

    Version 0 de calculette

---
+++ TDD/Calcultette/src/main/java/net/cofares/ljug/tdd/Calculette.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.cofares.ljug.tdd;
+
+/**
+ *
+ * @author pfares
+ */
+public class Calculette {
+    public int add (int x, int y) {
+        return x+y;
+    }
+    public int sub (int x, int y) {
+        return x-y;
+    }
+}
